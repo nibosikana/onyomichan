0104a0958a9695c487ca58883fbaa710509a75d3
ellreka <kozakana333@gmail.com>
2018-09-07 00:59:18 +0900

    contentscript_01

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,10 @@
-$(window).on("load", () => {
+//ウィンドウを閉じたとき
+$(window).on("beforeunload", () => {
+  window.speechSynthesis.cancel();
+  mo.disconnect();
+});
 
+$(window).on("load", () => {
   //ボタン画像
   const buttonIcon = (path) => {
     return chrome.extension.getURL(path);
@@ -7,11 +12,11 @@ $(window).on("load", () => {
 
   //ボタンを追加
   $('body').append('<div id="buttonBox-onyomichan"></div>');
-  $('#buttonBox-onyomichan').append(`<span id="playButton-onyomichan" v-show="isActive" v-on:click="play"><img src="{{ playIcon }}"/></span>`)
-  $('#buttonBox-onyomichan').append(`<span id="pauseButton-onyomichan"v-show="!isActive" v-on:click="pause"><img src="{{ pauseIcon }}"/></span>`)
+  $('#buttonBox-onyomichan').append(`<span id="playButton-onyomichan" v-show="pActive" v-bind:style="pStyle" v-on:click="play"><img src="{{ playIcon }}"/></span>`)
+  $('#buttonBox-onyomichan').append(`<span id="pauseButton-onyomichan"v-show="!pActive" v-on:click="pause"><img src="{{ pauseIcon }}"/></span>`)
   $('#buttonBox-onyomichan').append(`<span id="stopButton-onyomichan" v-on:click="stop"><img src="{{ stopIcon }}"/></span>`)
-  $('#buttonBox-onyomichan').append(`<span id="eyeButton-onyomichan" v-on:click="eye"><img src="{{ eyeIcon }}"/></span>`)
-  $('#buttonBox-onyomichan').append('<span id="slashButton-onyomichan" v-on:click="slash">Slash</span>')
+  $('#buttonBox-onyomichan').append(`<span id="eyeButton-onyomichan" v-show="eActive" v-bind:style="eStyle" v-on:click="eye"><img src="{{ eyeIcon }}"/></span>`)
+  $('#buttonBox-onyomichan').append('<span id="redeyeButton-onyomichan" v-show="!eActive" v-bind:style="eStyle" v-on:click="redeye"><img src="{{ redeyeIcon }}"/></span>')
 
   //Clickイベント
   const buttonEvent = new Vue({
@@ -21,26 +26,50 @@ $(window).on("load", () => {
       pauseIcon: buttonIcon('images/pause.svg'),
       stopIcon: buttonIcon('images/stop.svg'),
       eyeIcon: buttonIcon('images/eye.svg'),
-      isActive: true
+      redeyeIcon: buttonIcon('images/redeye.svg'),
+      pStyle: {
+        'pointer-events': 'auto',
+      },
+      eStyle: {
+        'pointer-events': 'auto',
+      },
+      pActive: true,
+      eActive: true
     },
     methods: {
-      play: function(event) {
-        this.isActive = !this.isActive
-        playEvent()
-
+      play: function() {
+        this.pActive = false
+        this.eActive = false
+        this.eStyle['pointer-events'] = 'none'
+        if(window.speechSynthesis.speaking){
+          window.speechSynthesis.resume();
+        }else{
+          playEvent()
+        }
       },
-      pause: function(event) {
-        this.isActive = !this.isActive
-        console.log(this.pauseIcon)
+      pause: function() {
+        this.pActive = true
+        window.speechSynthesis.pause();
       },
-      stop: function(event) {
-        console.log(event)
+      stop: function() {
+        this.pActive = true
+        this.eActive = true
+        this.pStyle['pointer-events'] = 'auto'
+        this.eStyle['pointer-events'] = 'auto'
+        window.speechSynthesis.cancel();
+        mo.disconnect();
       },
-      eye: function(event) {
-        console.log(event)
+      eye: function() {
+        this.eActive = false
+        this.pStyle['pointer-events'] = 'none'
+        newResponse()
       },
-      slash: function(event) {
-        console.log(event)
+      redeye: function() {
+        this.eActive = true
+        this.pStyle['pointer-events'] = 'auto'
+        this.eStyle['pointer-events'] = 'auto'
+        window.speechSynthesis.cancel();
+        mo.disconnect();
       }
     }
   })
@@ -53,26 +82,20 @@ const playEvent = () => {
     before: [/2/g,/>>/g],
     after: ["ああああああああああああ","ううううううううう"]
   }
-  //レスを取得
   let RES = []
   $('dd').each(function( index ) {
     RES.push($(this.outerHTML).children('ares').empty().parent().text());
   });
   console.log(RES)
-  console.log(replaceText(RES,repWords))
-
-  var ss = new SpeechSynthesisUtterance();
-
-  ss.text = replaceText(RES,repWords)
-  ss.rate = 1;
-  ss.pitch = 1;
-  ss.volume = 1;
-  ss.lang = 'ja'
-  speechSynthesis.speak( ss );
-  console.log(ss)
+  const pText = replaceText(RES, repWords);
+  console.log(pText)
+  pText.forEach(function(val){
+    speechSynthesis(val);
+  })
+  newResponse();
 }
 
-//文字置換
+//置換
 const replaceText = (text,word) => {
   const resArr = text.map((value) => {
     i = 0;
@@ -86,137 +109,24 @@ const replaceText = (text,word) => {
 
 }
 
+//新しいレスを監視
+const newResponse = () => {
+  const target = document.getElementsByClassName('thread')[0];
+  mo = new MutationObserver((data) => {
+    let nText = $(data[0].addedNodes[0].innerHTML).children('ares').empty().parent().text()
+    speechSynthesis(nText)
+  })
+  mo.observe(target,{childList:true})
+}
 
-
-//  //html読み込み
-//  $.get(chrome.extension.getURL('button.html'), function (data) {
-//   $($.parseHTML(data)).appendTo('body');
-// });
-// window.addEventListener('beforeunload', function () {
-//   speechSynthesis.cancel();
-//   observer.disconnect();
-// });
-// $(document).ready(function () {
-//   eye = document.getElementById("eye-btn")
-//   slash = document.getElementById("slash-btn")
-//   stop = document.getElementById("stop-btn")
-//   var start = new Vue({
-//     el: '.button-box',
-//     data: {
-//       seen: true
-//     },
-//     methods: {
-//       start: function () {
-//         this.seen = false
-//         if (speechSynthesis.speaking == true) {
-//           speechSynthesis.resume();
-//         } else {
-//           eye.style = "color:red";
-//           eye.disabled = "true";
-//           slash.disabled = 'true';
-//           startEvent();
-//           Mutation()
-//         }
-//       },
-//       pause: function () {
-//         this.seen = true
-//         if (speechSynthesis.speaking == true) {
-//           speechSynthesis.pause();
-//         } else {
-//           speechSynthesis.cancel();
-//         }
-//       },
-//       stop: function () {
-//         this.seen = true
-//         eye.style.color = "";
-//         eye.disabled = "";
-//         slash.disabled = '';
-//         speechSynthesis.cancel();
-//         observer.disconnect();
-//       },
-//       eye: function () {
-//         sb = document.getElementById("start-btn")
-//         Mutation();
-//         eye.style = "color:red";
-//         eye.disabled = 'true';
-//         sb.disabled = 'true';
-//         stop.disabled = 'true';
-//       },
-//       slash: function () {
-//         this.seen = true
-//         eye.style.color = "";
-//         eye.disabled = "";
-//         sb.disabled = '';
-//         stop.disabled = '';
-//         speechSynthesis.cancel();
-//         observer.disconnect();
-//       }
-//     }
-//   })
-
-//   function startEvent() {
-//     chrome.storage.sync.get(function (result) {
-//       speechSynthesis.cancel();
-//       var num = $("#number").val()
-//       var ares = $(`ares[num=${num}]`)[0];
-//       var index = $("ares").get().indexOf(ares)
-//       var last = $("dd").length
-//       var dd = $("dd").slice(index, last);
-//       var title = $("h1").get(0).textContent;
-//       var res = dd.map(function (value, array) {
-//         var t = $(array.outerHTML).children('ares').empty().parent().text();
-//         if (t.length - 2 < result.other[1]) {
-//           return t;
-//         } else {}
-//       })
-//       var ss = new SpeechSynthesisUtterance();
-//       if (result.other[0] == true) {
-//         text = title
-//         text += $.makeArray(res).join("");
-//         Replace(result);
-//         ss.text = text;
-//       } else {
-//         text = $.makeArray(res).join("");
-//         Replace(result);
-//         ss.text = text;
-//       }
-//       ss.rate = result.voices[0];
-//       ss.pitch = result.voices[1];
-//       ss.volume = result.voices[2];
-//       ss.lang = 'ja';
-//       speechSynthesis.speak(ss);
-//       //console.log(ss);
-//     })
-//   }
-
-//   function Mutation() {
-//     var target = $('.thread').get(0);
-//     observer = new MutationObserver(function () {
-//       chrome.storage.sync.get(function (result) {
-//         text = $($("dd").last()[0].outerHTML).children('ares').empty().parent().text();
-//         Replace(result)
-//         var ss = new SpeechSynthesisUtterance();
-//         ss.rate = result.voices[0];
-//         ss.pitch = result.voices[1];
-//         ss.volume = result.voices[2];
-//         ss.lang = 'ja';
-//         ss.text = text;
-//         speechSynthesis.speak(ss);
-//         console.log(ss);
-//       });
-//     })
-//     var config = {
-//       childList: true
-//     }
-//     observer.observe(target, config)
-//   }
-
-//   function Replace(result) {
-//     for (var k = 0; k < result.obj.before.length; k++) {
-//       var targetStr = result.obj.before[k];
-//       var bef = new RegExp(targetStr, "g");
-//       var aft = result.obj.after[k];
-//       text = text.replace(bef, aft);
-//     }
-//   }
-// })
+//読み上げる
+const speechSynthesis = (ssText) => {
+  let ss = new SpeechSynthesisUtterance();
+  ss.text = ssText;
+  ss.rate = 1;
+  ss.pitch = 1;
+  ss.volume = 0.2;
+  ss.lang = 'ja';
+  console.log(ss);
+  window.speechSynthesis.speak(ss);
+}
